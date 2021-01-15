db054a549d614ab56d7b9063a1134c20bcc922a4
maitaoguo <guomaitao@gmail.com>
2021-01-15 14:56:38 -0800

    updates

--- src/components/Results.js
+++ src/components/Results.js
@@ -10,6 +10,10 @@ import {
 } from "react-icons/fa";
 import Card from "./Card";
 import PropTypes from "prop-types";
+import Loading from "./Loading";
+import Tooltip from "./Tooltip";
+import queryString from "query-string";
+import { Link } from "react-router-dom";
 
 function ProfileList({ profile }) {
   return (
@@ -20,14 +24,18 @@ function ProfileList({ profile }) {
       </li>
       {profile.location && (
         <li>
-          <FaCompass color="rgb(144, 115, 255)" size={22} />
-          {profile.location}
+          <Tooltip text="User's location">
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
+            {profile.location}
+          </Tooltip>
         </li>
       )}
       {profile.company && (
         <li>
-          <FaBriefcase color="#795548" size={22} />
-          {profile.company}
+          <Tooltip text="User's company">
+            <FaBriefcase color="#795548" size={22} />
+            {profile.company}
+          </Tooltip>
         </li>
       )}
       <li>
@@ -58,7 +66,10 @@ export default class Results extends React.Component {
     };
   }
   componentDidMount() {
-    const { playerOne, playerTwo } = this.props;
+    const { playerOne, playerTwo } = queryString.parse(
+      this.props.location.search
+    );
+
     battle([playerOne, playerTwo])
       .then((players) => {
         this.setState({
@@ -80,7 +91,7 @@ export default class Results extends React.Component {
     const { winner, loser, error, loading } = this.state;
 
     if (loading === true) {
-      return <p>Loading</p>;
+      return <loading text="Battling" />;
     }
 
     if (error) {
@@ -109,9 +120,9 @@ export default class Results extends React.Component {
             <ProfileList profile={loser.profile} />
           </Card>
         </div>
-        <button className="btn dark-btn btn-space" onClick={this.props.onReset}>
+        <Link className="btn dark-btn btn-space" to="/battle">
           Reset
-        </button>
+        </Link>
       </React.Fragment>
     );
   }
